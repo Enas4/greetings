2e3b393eba1fe978792d514942bac7b5945e393c
Enas4 <harleydavidson789@icloud.com>
2022-06-07 15:58:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let time = new Date();
+  let hr = time.getHours();
+  let greetings = "";
+  const cssStyle = {};
+
+  if (hr > 1 && hr < 12) {
+    greetings = "Good Morning !";
+    cssStyle.color = "green";
+  } else if (hr > 12 && hr <= 16) {
+    greetings = "Good Afternoon !";
+    cssStyle.color = "orange";
+  } else if (hr > 16 && hr <= 19) {
+    greetings = "Good Evening !";
+    cssStyle.color = "blue";
+  } else if (hr > 19 && hr <= 24) {
+    greetings = "Good night !";
+    cssStyle.color = "black";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello Everyone, <span style={cssStyle}>{greetings}</span>
+      </h1>
     </div>
   );
 }
